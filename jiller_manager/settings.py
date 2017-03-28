cf892bb924f708a590355304ab78063a9dfdf974
amerigoman <oleksandrdubrovskyi@gmail.com>
2017-03-28 14:47:01 +0300

    paypal completed

--- jiller_manager/settings.py
+++ jiller_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$hslqb$o(_e#nx&97%w*hbayes@a04^7p627ap137o^so=!6hs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['90c66a44.ngrok.io', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['66ddefb1.ngrok.io', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -125,7 +125,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # to use PAYPAL sandbox
 PAYPAL_TEST = True
-PAYPAL_RECEIVER_EMAIL = 'colonialist@ukr.net'
+PAYPAL_RECEIVER_EMAIL = 'colonialist-facilitator@ukr.net'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
